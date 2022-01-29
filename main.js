847520887b0a061b78e185be3664625af667de82
PrasanthChowhan <57288401+PrasanthChowhan@users.noreply.github.com>
2022-01-29 07:02:59 +0530

    searchbar added with animation

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 // opening menu in mobile 
 const menuEl = document.querySelector(".mobileMenu")
 const headerRightEl = document.querySelector(".header-right")
+const searchEl = document.querySelector(".search")
+const searchContEl = document.querySelector(".search-container")
 
 menuEl.addEventListener("click", function(){
     headerRightEl.classList.toggle("active")
     menuEl.classList.toggle("close")
+    // adding comment here
+})
+searchEl.addEventListener("click", function(){
+    searchContEl.classList.toggle("click")
     // adding comment here
 })
